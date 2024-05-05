079da6e03551f8544cab4284d42f1bbefc057bd9
Shuddhodhan Surwade <119784108+sidz111@users.noreply.github.com>
2024-05-05 09:43:50 +0530

    Time: 82 ms (19.57%), Space: 45.3 MB (15.25%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.java
+++ 0514-freedom-trail/0514-freedom-trail.java
@@ -8,12 +8,16 @@ class Solution {
   private int dfs(final String ring, final String key, int index, Map<String, Integer> mem) {
     if (index == key.length())
       return 0;
+    // Add the index to prevent duplication.
     final String hashKey = ring + index;
     if (mem.containsKey(hashKey))
       return mem.get(hashKey);
 
     int ans = Integer.MAX_VALUE;
 
+    // For each ring[i] == key[index], we rotate the ring to match the ring[i]
+    // with the key[index], then recursively match the newRing with the
+    // key[index + 1..n).
     for (int i = 0; i < ring.length(); ++i)
       if (ring.charAt(i) == key.charAt(index)) {
         final int minRotates = Math.min(i, ring.length() - i);
