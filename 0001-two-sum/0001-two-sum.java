aedb2592c88639e24dc5b8e3418075fc74bc46f2
Shuddhodhan Surwade <119784108+sidz111@users.noreply.github.com>
2025-08-27 00:24:53 +0530

    Time: 38 ms (44.25%), Space: 45 MB (47.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,14 @@
 class Solution {
-  public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-
-    for (int i = 0; i < nums.length; ++i) {
-      if (map.containsKey(target - nums[i]))
-        return new int[] {map.get(target - nums[i]), i};
-      map.put(nums[i], i);
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[j]==target-nums[i]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
     }
-
-    throw new IllegalArgumentException();
-  }
 }
